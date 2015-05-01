472faddb964dd3eacd08feec93af38359127b5fc
jianminglok <jianminglok@hotmail.com>
2015-05-01 15:41:44 +0800

    fixes

--- omni.mk
+++ omni.mk
@@ -5,14 +5,14 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 PRODUCT_RELEASE_NAME := A526
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/A526/device_A526.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A526
-PRODUCT_NAME := cm_A526
+PRODUCT_NAME := omni_A526
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := A526
 PRODUCT_MANUFACTURER := Lenovo
